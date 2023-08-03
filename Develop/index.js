6b2753000adbecffec109f5d3ee612737e98a87d
unknown <danleviobello@gmail.com>
2023-08-03 18:32:53 -0500

    added some more questions

--- Develop/index.js
+++ Develop/index.js
@@ -8,13 +8,24 @@ const generateMarkdown = require('./utils/generateMarkdown.js')
 
 // TODO: Create an array of questions for user input
 const questions = [{
-    type: 'input',
-    name: 'description',
-    message: 'Provide a description of the project (Required)',
+    type: 'confirm ',
+    name: 'begin',
+    message: 'Are you ready to create a README file?',
 },
 {
     type: 'input',
-    
+    name: 'title',
+    message: 'What is the title of your project?',
+},
+{
+    type: 'input',
+    name: 'description',
+    message: 'Please provide a description of your project.',
+},
+{
+    type: 'input',
+    name: 'table of contents',
+    message: 'Please provide a Table of Contents for your project. If there is no Table of Contents, please type "N/A".',
 }
 ];
 
