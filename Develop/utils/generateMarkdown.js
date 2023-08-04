b95b787781ccf3addffdd90af23750494130c4c7
unknown <danleviobello@gmail.com>
2023-08-03 23:18:15 -0500

    Cleaned up files, like removing TODOs

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,40 +1,28 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(responses) {
     return `# ${responses.title}
 
-    ## Description
+## Description
 
     ${responses.description}
 
-    ### Installation
+### Installation
     
     ${responses.installation}
 
-    ### Usage Information
+### Usage Information
 
     ${responses.usage}
 
-    ### Contribution Guidelines
+### Contribution Guidelines
 
     ${responses.contributing}
 
-    ## Table of Contents
+## Table of Contents
 
     ${responses.tableOfContents}
 
-    ## Questions
+## Questions
 
     Here is my GitHub: ${responses.github}
     And here is my email: ${responses.email}
